0321a565b614d2208df8cd8db96350b5722d2f89
Florian Bieringer <florian.bieringer@uni-passau.de>
2016-08-10 11:04:49 +0200

    Added input

--- views/show/message.php
+++ views/show/message.php
@@ -1,3 +1,4 @@
+<div class="messages">
 <h2><?= htmlReady($topic) ?></h2>
 <? if ($id): ?>
     <a href="<?= URLHelper::getLink("dispatch.php/messages/write", array('answer_to' => $id)) ?>"
@@ -16,3 +17,8 @@
         <?= formatReady($msg['message']) ?>
     </article>
 <? endforeach; ?>
+</div>
+<div class="message_answer">
+    <textarea name="answer_text"></textarea>
+    <?= \Studip\Button::create(_('Absenden')) ?>
+</div>
